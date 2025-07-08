30583dd758196f052d332ce86b1f4de56b23ee47
Şamil Nugay <samil_nugay@hotmail.com>
2025-07-08 21:22:02 +0300

    second

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('157141945d030dffa7d5af42b1afedd84e139b5199390a919bcac637f4e8cb04'),
+    keys: env.array('157141945d030dffa7d5af42b1afedd84e139b5199390a919bcac637f4e8cb04,dcde0bac6bedad20a81765c3782b7e513457de56c1d53c3cf6cfaa89f54d6350'),
   },
 });
